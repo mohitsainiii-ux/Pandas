a4f7d8ce01061f0e6f64b6ded02b780125fd8645
MOHIT SAINI <mohitsaini600300@gmail.com>
2026-09-04 22:32:38 +0530

    y

---
+++ Q14.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+print(df)
+
+data = pd.DataFrame({
+    "Date": ["2023-01-01", "2023-01-02", "2023-01-03"],
+    "Value": [10, 20, 30]
+})
+print(data)
